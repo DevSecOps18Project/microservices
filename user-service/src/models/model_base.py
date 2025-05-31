999fe10269964de2a11a0fc681063230fa41fba7
Yaakov Neuman <yakinew@yahoo.com>
2025-05-31 22:57:11 +0300

    feat(models) : Rollback DB on failure in create and update
    In case the methods create or update fails with error, we should
    rollback the DB here, in the base model, and not the in controller
    hadnler functions.
    
    Issue: #7
    
    Signed-off-by: Yaakov Neuman <yakinew@yahoo.com>

--- user-service/src/models/model_base.py
+++ user-service/src/models/model_base.py
@@ -1,7 +1,9 @@
 """
 SQLAlchemy models for the Base Model
 """
+import logging
 from sqlalchemy import Column, Integer
+from sqlalchemy.exc import IntegrityError
 from database import db_session
 
 
@@ -13,14 +15,32 @@ class BaseModel:
     def to_dict(self):
         raise NotImplementedError(f'Please implement fhe function for {self.__class__.__name__}')
 
-    def create(self, commit=True):
-        db_session.add(self)
-        if commit:
-            db_session.commit()
+    def create(self, commit=True) -> bool:
+        try:
+            db_session.add(self)
+            if commit:
+                db_session.commit()
+            return True
+        except IntegrityError:
+            db_session.rollback()
+            return False
+        except Exception as e:  # pylint: disable=W0718
+            logging.exception(f'create : unexpected exception : {e}')
+            db_session.rollback()
+            raise e
 
-    def update(self, _: dict, commit=True):
-        if commit:
-            db_session.commit()
+    def update(self, _: dict, commit=True) -> bool:
+        try:
+            if commit:
+                db_session.commit()
+            return True
+        except IntegrityError:
+            db_session.rollback()
+            return False
+        except Exception as e:  # pylint: disable=W0718
+            logging.exception(f'update : unexpected exception : {e}')
+            db_session.rollback()
+            raise e
 
     def delete(self, commit=True):
         db_session.delete(self)
